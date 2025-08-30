7d67a51f7acc1f8040b5b6ccd047548cfafb20b9
nemmarton <103062588+nemmarton@users.noreply.github.com>
2025-08-30 13:32:58 +0200

    Update fifth_task.c

--- fifth_task.c
+++ fifth_task.c
@@ -1,10 +1,13 @@
-/* Task description:
+/* Task description:Inserting strings into strings
 
+This task is similar to the previous ones: the function receives a string, and creates a new one.
 
+This time, however, the task is not to remove/cut something from a string, but to insert another string to somewhere into the original string. 
+For instance, the result of insertstring("hello!", 5, ", world") is a new string containing hello, world!: the second string has been inserted between letters "o" and "!".
 */
 
 
-#include <stdio.h>
+#include <stdio.h>//don't forget to include the required libraries
 
 int main(){
 
